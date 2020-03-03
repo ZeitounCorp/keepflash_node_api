5acd35bd11221d6bfb3e25a4b580f9af0b891f7d
Lenny Zeitoun <zeitounlenny@gmail.com>
2020-03-04 01:59:53 +0200

    Version Bump

--- publish.sh
+++ publish.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
 npm version patch
+git add .
+git commit -m "Version Bump"
+git push -u origin master
 npm publish --access public
 osascript -e 'display notification \"Et Voilà\" with title \"Publish finished running\" subtitle \"I finished publishing your library to npmjs\" sound name \"Purr\"'
